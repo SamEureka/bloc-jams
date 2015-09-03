28abc2a8afdf5f3ea156bd2458222d72ad7ef6cd
SamEureka <sameureka@me.com>
2015-09-03 09:07:14 -0400

    Checkpoint 26 - Play/Pause pt1, Complete

--- scripts/album.js
+++ scripts/album.js
@@ -60,7 +60,10 @@ var albumMarconi = {
 
 var createSongRow = function (songNumber, songName, songLength) {
     var template =
-        '<tr class="album-view-song-item">' + '<td class="song-item-number">' + songNumber + '</td>' + '<td class="song-item-title">' + songName + '</td>' + '<td class="song-item-duration">' + songLength + '</td>' + '</tr>';
+        '<tr class="album-view-song-item">' 
+    + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
+
+    + '<td class="song-item-title">' + songName + '</td>' + '<td class="song-item-duration">' + songLength + '</td>' + '</tr>';
     return template;
 };
 
@@ -78,11 +81,28 @@ var setCurrentAlbum = function (album) {
 
     albumSongList.innerHTML = '';
 
-    for (i = 0; i < album.songs.length; i++) {
+    for (var i = 0; i < album.songs.length; i++) {
         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].name, album.songs[i].length);
     }
 };
+// Comment about listeners
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+// button template
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 
 window.onload = function () {
-    setCurrentAlbum(albumPicasso);
+        setCurrentAlbum(albumPicasso);
+
+        songListContainer.addEventListener('mouseover', function (event) {
+            if (event.target.parentElement.className === 'album-view-song-item') {
+                event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+            }
+        });
+for (i = 0; i < songRows.length; i++) {
+            songRows[i].addEventListener('mouseleave', function (event) {
+                this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+            });
 };
+        };
