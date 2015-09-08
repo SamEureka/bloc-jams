6fa6b2b35b8ab94024239721488bdb054b990570
Sam Eureka <sameureka@me.com>
2015-09-08 10:22:08 -0400

    Checkpoint 29, complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,31 +1,27 @@
-var pointsArray = document.getElementsByClassName('point');
-
-var animatePoints = function (points) {
+var animatePoints = function () {
     
-
-    var revealPoint = function () {
-        for (var i in points) {
-            points[i].style.opacity = 1;
-            points[i].style.transform = "rotate(7deg) scaleX(1) translateY(0)";
-            points[i].style.msTransform = "rotate(7deg) scaleX(1) translateY(0)";
-            points[i].style.WebkitTransform = "rotate(7deg) scaleX(1) translateY(0)";
-        }
+    var revealPoint = function() {
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
     };
-
-    revealPoint();
+      $.each($('.point'), revealPoint);  
+    
 };
-window.onload = function() {
+
+$(window).load(function() {
     
     // had to change height from 950 to 850 //Sam
-    if (window.innerHeight > 850) {
-    animatePoints(pointsArray);
+    if ($(window).height() > 850) {
+    animatePoints();
     }
     
-window.addEventListener('scroll', function(event) {
+$(window).scroll(function(event) {
 
-    if (pointsArray[0].getBoundingClientRect().top <= 400){
-        animatePoints(pointsArray);
+    if ($(window).scrollTop() >= 400){
+        animatePoints();
     }
     
 });
-}
+});
